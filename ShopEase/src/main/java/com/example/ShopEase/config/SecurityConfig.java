37436d5bcf2219f99397d22492017efbec60f365
NicolasD1440 <nicolascastiblanco6@gmail.com>
2025-03-12 20:28:44 -0500

    Primer commit - subida inicial

---
+++ ShopEase/src/main/java/com/example/ShopEase/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.ShopEase.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // Permite acceso a archivos estáticos
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
